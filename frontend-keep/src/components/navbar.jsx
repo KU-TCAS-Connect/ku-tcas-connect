791210f17d73b93d3b205b71ae5606b47acfcd7f
panitnt <panittatanya@gmail.com>
2025-02-14 00:55:30 +0700

    Initial commit

---
+++ frontend-keep/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="px-8 py-4 shadow-sm">
+            <h2 className='text-kutcas-green-700 text-b text-lg font-bold'>KUTCAS connect</h2>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChatPage from "./chatapp/chatapp";
+
+function App() {
+  return (
+    <div>
+      <ChatPage />
+    </div>
+  );
+}
+
+export default App;

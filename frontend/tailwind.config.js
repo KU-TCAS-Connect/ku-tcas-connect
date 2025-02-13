227eaad81d157f24f28f0cac493db735de43a13b
panitnt <panittatanya@gmail.com>
2025-02-14 01:39:00 +0700

    Update

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ], // Update this based on your file structure
+  theme: {
+    extend: {
+      colors: {
+        'kutcas-green-100':'#D9EBEE',
+        'kutcas-green-700' : "#0097B2",
+      },
+    },
+  },
+  plugins: [],
+};
